8bf0b8904c8266a514b1ed2c4305eefe8c5ae051
Jakob Albrecht <mrwylan@gmail.com>
2025-04-18 15:42:42 +0200

    init for the well known colors demo mcp server

--- src/main/java/info/wylan/mymcp/ColorTranslationService.java
+++ src/main/java/info/wylan/mymcp/ColorTranslationService.java
@@ -5,9 +5,7 @@ import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class ColorTranslationService {

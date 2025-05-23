14dc9904d40fd64c3d7ac8156a4aefc85fbafb2f
Jakob Albrecht <jakob.albrecht@transgourmet.ch>
2025-05-23 13:48:55 +0200

    fixed package import

--- src/main/java/info/wylan/mymcp/MyMcpServerApplication.java
+++ src/main/java/info/wylan/mymcp/MyMcpServerApplication.java
@@ -1,7 +1,7 @@
 package info.wylan.mymcp;
 
+import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
-import org.springframework.ai.tool.ToolCallbacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
